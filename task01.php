85194fe5e9e1c5acfe30b7ba298034d47c0b3c8e
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-04 10:42:50 +0200

    maj task01

--- task01.php
+++ task01.php
@@ -2,9 +2,9 @@
 
 function discover_type(int $age, string $name, float $gpa, bool $isStudent) {
         if ($isStudent) {
-            echo "Hello my name is $name, I’m $age years old. I’m a student at Epitech with a GPA of $gpa.".PHP_EOL;
+            echo "Hello my name is $name, I’m $age years old. I’m a student at Epitech with a GPA of $gpa.\n";
         } else {
-            echo "Hello my name is $name, I’m $age years old. I’m not a student.".PHP_EOL;
+            echo "Hello my name is $name, I’m $age years old. I’m not a student.\n";
         }
 }
 
