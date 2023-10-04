fdb8eab3e389154eebbede13f311d29d320e812c
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-04 11:44:41 +0200

    End of task06

--- task06.php
+++ task06.php
@@ -2,27 +2,34 @@
 
 function sequence($n)
 {
-    if ($n < 1) { return "1";}
+    if ($n < 1) {
+        echo "1\n";
+        return;
+    }
 
     $lastLine = "1";
 
     for ($i = 0; $i < $n; $i++) {
 
         echo $lastLine . "\n";
-        
+
+        $newLine = "";
         $tab = [];
 
-        foreach(str_split($lastLine) as $char) {
+        foreach (str_split($lastLine) as $char) {
             if (empty($tab) || $tab[count($tab) - 1][0] != $char) {
                 $tab[] = [$char, 1];
             } else {
                 $tab[count($tab) - 1][1]++;
             }
         }
-        
+
+        foreach ($tab as $item) {
+            $newLine .= $item[1] . $item[0];
+        }
+
+        $lastLine = $newLine;
     }
 
-    foreach($tab as $item) {
-        echo $item[1] . $item[0];
-    }
+    echo $lastLine . "\n";
 }
